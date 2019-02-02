7c560ea1c4cc411ea15fc1f9d763b9f0ef3119e9
Your Name <you@example.com>
2019-02-02 18:58:38 +0800

    version2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div>我爱你啊真快乐</div>
+            </div>
+        );
+    }
+}
+
+export default App;
